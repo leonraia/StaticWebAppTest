b12d3b20e9218499908412f5d4396d0c10f2c9eb
Raia Mauro Leonel <leonraia@quilmes.com.ar>
2021-05-28 19:06:20 -0300

    Test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+	<title>Index file</title>
+	<style>
+		body {
+			background-color: #2D2D2D;
+		}
+		
+		h1 {
+			color: #C26356;
+			font-size: 30px;
+			font-family: Menlo, Monaco, fixed-width;
+		}
+		
+		p {
+			color: white;
+			font-family: "Source Code Pro", Menlo, Monaco, fixed-width;
+		}
+	</style>
+</head>
+<body>
+	<h1>Index file</h1>
+	<p>Éxito</p>
+    <p><?php echo 'Funciona PHP'; ?></p>
+</body>
+</html>
---
+++ view/view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+	<title>View file</title>
+	<style>
+		body {
+			background-color: #2D2D2D;
+		}
+		
+		h1 {
+			color: #C26356;
+			font-size: 30px;
+			font-family: Menlo, Monaco, fixed-width;
+		}
+		
+		p {
+			color: white;
+			font-family: "Source Code Pro", Menlo, Monaco, fixed-width;
+		}
+	</style>
+</head>
+<body>
+	<h1>View file</h1>
+	<p>Éxito</p>
+</body>
+</html>
